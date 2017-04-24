37261ab9d0211b6977633bb068d56ebebbed291a
Mario Octavio Jimenez <jimenez303@gmail.com>
2017-04-24 16:43:08 -0700

    changes to allow cross browser compatibility

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-const appController = (function AppController() {
-    const DOMSelector = {
+var appController = (function AppController() {
+    var DOMSelector = {
         close: '.js-close-modal',
         open: '.js-open-modal',
         modal: '.js-modal',
@@ -25,7 +25,9 @@ const appController = (function AppController() {
     function closeModal(event) {
         event.preventDefault();
         event.stopPropagation();
+        // Reset initial values
         document.querySelector(DOMSelector.modal).style.display = 'none';
+        document.querySelector(DOMSelector.progress).style.width = '';
     }
 
     function openModal(event) {
@@ -36,18 +38,21 @@ const appController = (function AppController() {
     }
 
     function setValues() {
-        let reached = document.querySelector(DOMSelector.reached).value;
-        let target = document.querySelector(DOMSelector.target).value;
+        var reached = parseFloat(document.querySelector(DOMSelector.reached).value);
+        var target = parseFloat(document.querySelector(DOMSelector.target).value);
+
+        // Fail save for max values
+        if (reached > target) reached = target;
 
         document.querySelector(DOMSelector.targetLabel).innerHTML = target;
-        document.querySelector(DOMSelector.targetLeft).innerHTML = target-reached;
+        document.querySelector(DOMSelector.targetLeft).innerHTML = target - reached;
         document.querySelector(DOMSelector.progressValue).innerHTML = reached;
         reached = Math.round(reached / target * 100);
         document.querySelector(DOMSelector.progressTooltip).style.paddingLeft = 250 * (reached / 100) + 'px';
 
-        setTimeout(function () {
+        setTimeout(function() {
             document.querySelector(DOMSelector.progress).style.width = reached + '%';
-        },200);
+        }, 200);
     }
 
     function _init() {
